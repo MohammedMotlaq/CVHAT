91582a770da52138443a67aa6387970d86037331
Mohammed Abu Motlaq <85284985+MohammedMotlaq@users.noreply.github.com>
2025-03-04 15:18:37 +0200

    update: splash screen

--- lib/src/screens/splash_splash/splash_screen.dart
+++ lib/src/screens/splash_splash/splash_screen.dart
@@ -2,8 +2,21 @@ import 'package:cvhat/src/core/resources/app_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      //Navigation to next screen
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
