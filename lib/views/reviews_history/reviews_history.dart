ebb04fd6e3c2ed7b0e35e9f0c7f808cb52f132d3
Oday <oabumutlak@gmail.com>
2025-03-06 14:29:17 +0200

    feat: Reviews history page

---
+++ lib/views/reviews_history/reviews_history.dart
@@ -0,0 +1,59 @@
+import 'package:cvhat/app_router.dart';
+import 'package:cvhat/core/resources/app_colors.dart';
+import 'package:cvhat/data/dummy_data.dart';
+import 'package:cvhat/models/review_details.dart';
+import 'package:cvhat/views/home_screen/widgets/recent_reviews_list.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ReviewsHistory extends StatelessWidget {
+  const ReviewsHistory({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final List<ReviewDetails> AllHistory = DummyData.aiReviews
+        .map((json) => ReviewDetails.fromJson(json))
+        .toList();
+    return Scaffold(
+      appBar: HistoryAppBar(),
+      body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 18.w),
+          child: RecentReviewsList(
+            recentReviews: AllHistory,
+            height: 900,
+          )),
+    );
+  }
+}
+
+class HistoryAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const HistoryAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      leading: IconButton(
+          onPressed: () {
+            AppRouter.popWidget();
+          },
+          icon: Icon(
+            Icons.arrow_back_rounded,
+            size: 32.sp,
+            color: AppColors.secondary,
+          )),
+      title: Text(
+        "History",
+        style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: AppColors.secondary,
+            fontSize: 32.sp),
+      ),
+      centerTitle: true,
+    );
+  }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
