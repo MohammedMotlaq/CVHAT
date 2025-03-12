15545a50e1e361c5b86ad42f2f99556a408a667a
Oday <oabumutlak@gmail.com>
2025-03-12 16:26:42 +0200

    Add: Api BaseUrl and endpoints

---
+++ lib/constants/api_endpoints.dart
@@ -0,0 +1,7 @@
+class ApiEndPoints {
+  static const String baseUrl = "http://12.12.12.11:8080";
+
+  static const String userSignup = "$baseUrl/user/auth/signup";
+  static const String userLogin = "$baseUrl/user/auth/login";
+  static const String userLogout = "$baseUrl/user/auth/logout";
+}
