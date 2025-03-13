551074cab401f3f8621cf48e5b857b76e1d143d3
Oday <oabumutlak@gmail.com>
2025-03-13 12:12:23 +0200

    feate: sign Up logic with loader and toastification

--- lib/views/auth/register_screen.dart
+++ lib/views/auth/register_screen.dart
@@ -1,10 +1,14 @@
+import 'dart:ui';
+
 import 'package:cvhat/app_router.dart';
 import 'package:cvhat/core/resources/app_colors.dart';
+import 'package:cvhat/providers/auth_provider.dart';
 import 'package:cvhat/views/auth/widgets/confirm_password_widget.dart';
 import 'package:cvhat/views/auth/widgets/forget_password_widget.dart';
 import 'package:cvhat/views/auth/widgets/login_widget.dart';
 import 'package:cvhat/views/auth/widgets/otp_widget.dart';
 import 'package:cvhat/views/auth/widgets/signup_widget.dart';
+import 'package:cvhat/widgets/loader_blur_screen.dart';
 import 'package:cvhat/widgets/logo_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -22,47 +26,47 @@ class RegisterScreen extends StatefulWidget {
 class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.bgWhite,
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 75.h,
-            ),
-            const LogoWidget(width: 135, height: 142),
-            SizedBox(
-              height: 40.h,
-            ),
-            Container(
-              width: 345.w,
-              height: 540.h,
-              padding: EdgeInsets.fromLTRB(18.w, 32.h, 18.w, 0),
-              decoration: BoxDecoration(
-                color: AppColors.bgWhite,
-                borderRadius: BorderRadius.circular(14.r),
-                boxShadow: [
-                  BoxShadow(
-                    color: AppColors.bgBlack.withOpacity(0.2),
-                    blurRadius: 10,
-                    spreadRadius: 3,
-                    offset: const Offset(4, 4),
+    return Consumer2<UiProvider, AuthProvider>(
+        builder: (context, uiProvider, authProvider, child) {
+      return Scaffold(
+          backgroundColor: AppColors.bgWhite,
+          body: Stack(children: [
+            SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 75.h,
                   ),
+                  const LogoWidget(width: 135, height: 142),
+                  SizedBox(
+                    height: 40.h,
+                  ),
+                  Container(
+                      width: 345.w,
+                      height: 540.h,
+                      padding: EdgeInsets.fromLTRB(18.w, 32.h, 18.w, 0),
+                      decoration: BoxDecoration(
+                        color: AppColors.bgWhite,
+                        borderRadius: BorderRadius.circular(14.r),
+                        boxShadow: [
+                          BoxShadow(
+                            color: AppColors.bgBlack.withOpacity(0.2),
+                            blurRadius: 10,
+                            spreadRadius: 3,
+                            offset: const Offset(4, 4),
+                          ),
+                        ],
+                      ),
+                      child: AppRouter.animatedSwitcher(
+                        _getAuthWidget(uiProvider.authState),
+                      ))
                 ],
               ),
-              child: Consumer<UiProvider>(
-                builder: (context, uiProvider, child) {
-                  return AppRouter.animatedSwitcher(
-                    _getAuthWidget(uiProvider.authState),
-                  );
-                },
-              ),
-            )
-          ],
-        ),
-      ),
-    );
+            ),
+            if (authProvider.isLoading) LoaderBlurScreen()
+          ]));
+    });
   }
 }
 
