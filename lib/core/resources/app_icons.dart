2a51f4ffdbc86cc7f91ff2e24d382365ca7ed54f
Mohammed Abu Motlaq <85284985+MohammedMotlaq@users.noreply.github.com>
2025-03-13 14:54:48 +0200

    feat: add drawer icons

--- lib/core/resources/app_icons.dart
+++ lib/core/resources/app_icons.dart
@@ -1,6 +1,15 @@
 class AppIcons {
   static const String logo = "assets/images/logo.png";
-  static const String premium = "assets/images/premium.png";
+  static const String premiumOffer = "assets/images/premium.png";
   static const String cv = "assets/images/cv.png";
   static const String menu = "assets/icons/menu.png";
+  static const String profile = "assets/icons/profile.png";
+  static const String profileFilled = "assets/icons/profile_filled.png";
+  static const String home = "assets/icons/home.png";
+  static const String homeFilled = "assets/icons/home_filled.png";
+  static const String premium = "assets/icons/premium.png";
+  static const String premiumFilled = "assets/icons/premium_filled.png";
+  static const String favorite = "assets/icons/favorite.png";
+  static const String favoriteFilled = "assets/icons/favorite_filled.png";
+  static const String logout = "assets/icons/logout.png";
 }
