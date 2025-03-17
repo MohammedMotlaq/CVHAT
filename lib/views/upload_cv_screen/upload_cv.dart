b0ac3623c86cc3e286d533f8e27830d8790a262b
Oday <oabumutlak@gmail.com>
2025-03-17 11:33:42 +0200

    Fix: adaptation changes

--- lib/views/upload_cv_screen/upload_cv.dart
+++ lib/views/upload_cv_screen/upload_cv.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 
 import 'package:cvhat/app_router.dart';
 import 'package:cvhat/core/resources/app_colors.dart';
+import 'package:cvhat/providers/feedback_provider.dart';
 import 'package:cvhat/providers/file_picker_provider.dart';
 import 'package:cvhat/views/upload_cv_screen/widgets/upload_cv_app_bar.dart';
 import 'package:cvhat/widgets/custom_button.dart';
@@ -21,149 +22,156 @@ class UploadCv extends StatelessWidget {
 
     return PopScope(
       canPop: false,
-      child: Scaffold(
-        backgroundColor: AppColors.bgWhite,
-        appBar: const UploadCvAppBar(),
-        body: SizedBox(
-          width: 390.w,
-          child: Consumer<FilePickerProvider>(
-            builder: (context, filePickerProvider, child) {
-              return SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(
-                      height: 20.h,
-                    ),
-                    Material(
-                      child: GestureDetector(
-                        onTap: () {
-                          filePickerProvider
-                              .pickFile(allowedExtensions: ["pdf"]);
-                        },
-                        child: filePickerProvider.selectedFile == null
-                            ? CustomPaint(
-                                painter: DottedBorderPainter(
-                                  color: AppColors.secondary,
-                                  strokeWidth: 2,
-                                  gap: 6,
-                                  borderRadius: 20.r, // Adjust corner radius
-                                ),
-                                child: Container(
-                                  height: 300.h,
-                                  width: 300.w,
-                                  decoration: BoxDecoration(
-                                    color: AppColors.secondary
-                                        .withValues(alpha: 0.05),
-                                    borderRadius: BorderRadius.circular(20.r),
+      child: Consumer<FeedBackProvider>(
+          builder: (context, feedBackProvider, child) {
+        return Scaffold(
+          backgroundColor: AppColors.bgWhite,
+          appBar: const UploadCvAppBar(),
+          body: SizedBox(
+            width: 390.w,
+            child: Consumer<FilePickerProvider>(
+              builder: (context, filePickerProvider, child) {
+                return SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        height: 20.h,
+                      ),
+                      Material(
+                        child: GestureDetector(
+                          onTap: () {
+                            filePickerProvider
+                                .pickFile(allowedExtensions: ["pdf"]);
+                          },
+                          child: filePickerProvider.selectedFile == null
+                              ? CustomPaint(
+                                  painter: DottedBorderPainter(
+                                    color: AppColors.secondary,
+                                    strokeWidth: 2,
+                                    gap: 6,
+                                    borderRadius: 20.r, // Adjust corner radius
                                   ),
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.center,
-                                    children: [
-                                      Icon(
-                                        Icons.file_upload_outlined,
-                                        color: AppColors.secondary,
-                                        size: 48.sp,
-                                      ),
-                                      Text(
-                                        "Click To Upload",
-                                        style: TextStyle(
-                                            color: AppColors.secondary,
-                                            fontSize: 16.sp),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                              )
-                            : Stack(children: [
-                                Container(
+                                  child: Container(
                                     height: 300.h,
                                     width: 300.w,
                                     decoration: BoxDecoration(
-                                        color: AppColors.secondary
-                                            .withValues(alpha: 0.05),
+                                      color: AppColors.secondary
+                                          .withValues(alpha: 0.05),
+                                      borderRadius: BorderRadius.circular(20.r),
+                                    ),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        Icon(
+                                          Icons.file_upload_outlined,
+                                          color: AppColors.secondary,
+                                          size: 48.sp,
+                                        ),
+                                        Text(
+                                          "Click To Upload",
+                                          style: TextStyle(
+                                              color: AppColors.secondary,
+                                              fontSize: 16.sp),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                )
+                              : Stack(children: [
+                                  Container(
+                                      height: 300.h,
+                                      width: 300.w,
+                                      decoration: BoxDecoration(
+                                          color: AppColors.secondary
+                                              .withValues(alpha: 0.05),
+                                          borderRadius:
+                                              BorderRadius.circular(20.r),
+                                          border: Border.all()),
+                                      child: ClipRRect(
                                         borderRadius:
                                             BorderRadius.circular(20.r),
-                                        border: Border.all()),
-                                    child: ClipRRect(
-                                      borderRadius: BorderRadius.circular(20.r),
-                                      child: SfPdfViewer.file(
-                                        File(filePickerProvider
-                                            .selectedFile!.path!),
-                                        enableTextSelection: false,
-                                        maxZoomLevel: 1,
-                                        enableDoubleTapZooming: false,
-                                      ),
-                                    )),
-                                Positioned(
-                                    bottom: 10.w,
-                                    left: 0.w,
-                                    child: ElevatedButton(
-                                        style: ElevatedButton.styleFrom(
-                                            backgroundColor:
-                                                AppColors.secondary,
-                                            shape: const CircleBorder()),
-                                        child: Icon(
-                                          size: 24.sp,
-                                          Icons.clear,
-                                          color: AppColors.bgWhite,
+                                        child: SfPdfViewer.file(
+                                          File(filePickerProvider
+                                              .selectedFile!.path!),
+                                          enableTextSelection: false,
+                                          maxZoomLevel: 1,
+                                          enableDoubleTapZooming: false,
                                         ),
-                                        onPressed: () {
-                                          filePickerProvider.clearFile();
-                                        }))
-                              ]),
+                                      )),
+                                  Positioned(
+                                      bottom: 10.w,
+                                      left: 0.w,
+                                      child: ElevatedButton(
+                                          style: ElevatedButton.styleFrom(
+                                              backgroundColor:
+                                                  AppColors.secondary,
+                                              shape: const CircleBorder()),
+                                          child: Icon(
+                                            size: 24.sp,
+                                            Icons.clear,
+                                            color: AppColors.bgWhite,
+                                          ),
+                                          onPressed: () {
+                                            filePickerProvider.clearFile();
+                                          }))
+                                ]),
+                        ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 20.h,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 20.w),
-                      child: filePickerProvider.selectedFile != null
-                          ? Text(
-                              filePickerProvider.selectedFile!.name,
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 32.sp,
-                                  color: AppColors.secondary),
-                            )
-                          : const SizedBox(),
-                    ),
-                    SizedBox(
-                      height: 45.h,
-                    ),
-                    CustomTextField(
-                      width: 309,
-                      height: 50,
-                      textInputAction: TextInputAction.done,
-                      hintText: 'Submission Title',
-                      inputType: TextInputType.text,
-                      textEditingController: controller,
-                    ),
-                    SizedBox(
-                      height: 45.h,
-                    ),
-                    CustomButton(
-                      height: 55,
-                      width: 248,
-                      title: 'Submit',
-                      onTap: () {
-                        AppRouter.popWidget();
-                      },
-                    ),
-                    SizedBox(
-                      height: 20.h,
-                    )
-                  ],
-                ),
-              );
-            },
+                      SizedBox(
+                        height: 20.h,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20.w),
+                        child: filePickerProvider.selectedFile != null
+                            ? Text(
+                                filePickerProvider.selectedFile!.name,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 32.sp,
+                                    color: AppColors.secondary),
+                              )
+                            : const SizedBox(),
+                      ),
+                      SizedBox(
+                        height: 45.h,
+                      ),
+                      CustomTextField(
+                        width: 309,
+                        height: 50,
+                        textInputAction: TextInputAction.done,
+                        hintText: 'Submission Title',
+                        inputType: TextInputType.text,
+                        textEditingController:
+                            feedBackProvider.submitCvController,
+                      ),
+                      SizedBox(
+                        height: 45.h,
+                      ),
+                      CustomButton(
+                        height: 55,
+                        width: 248,
+                        title: 'Submit',
+                        onTap: () {
+                          feedBackProvider
+                              .postCV(filePickerProvider.selectedFile);
+                        },
+                      ),
+                      SizedBox(
+                        height: 20.h,
+                      )
+                    ],
+                  ),
+                );
+              },
+            ),
           ),
-        ),
-      ),
+        );
+      }),
     );
   }
 }
