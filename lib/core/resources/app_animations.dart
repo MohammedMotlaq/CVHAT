dcc567d0118de7ea7114584bb2f02f7677009721
Mohammed Abu Motlaq <85284985+MohammedMotlaq@users.noreply.github.com>
2025-03-17 14:50:18 +0200

    feat: add analyze animation

--- lib/core/resources/app_animations.dart
+++ lib/core/resources/app_animations.dart
@@ -2,4 +2,5 @@ class AppAnimations {
   static const String empty = "assets/animation/empty.json";
   static const String arrow = "assets/animation/arrow.json";
   static const String favorite = "assets/animation/favorite.json";
+  static const String analyze = "assets/animation/analyze.json";
 }
