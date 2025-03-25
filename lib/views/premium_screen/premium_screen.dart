6886e8704fd3b9f14b3dfa96a7877c32bde73b3c
Mohammed Abu Motlaq <85284985+MohammedMotlaq@users.noreply.github.com>
2025-03-25 09:14:09 +0200

    edit: premium offer image and coming soon

--- lib/views/premium_screen/premium_screen.dart
+++ lib/views/premium_screen/premium_screen.dart
@@ -3,6 +3,7 @@ import 'package:cvhat/core/resources/app_icons.dart';
 import 'package:cvhat/data/dummy_data.dart';
 import 'package:cvhat/models/premium_details.dart';
 import 'package:cvhat/views/premium_screen/widgets/premium_subscribe_list.dart';
+import 'package:cvhat/widgets/coming_soon.dart';
 import 'package:cvhat/widgets/custom_app_bar.dart';
 import 'package:cvhat/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
@@ -21,82 +22,80 @@ class PremiumScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.bgWhite,
       appBar: CustomAppBar(
-          title: "",
+          title: "Get Premium",
           bgColor: AppColors.bgWhite,
           textColor: AppColors.secondary,
           onTap: () {
             AppRouter.popWidget();
           }),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
+      body: Stack(
         children: [
-          SizedBox(
-            height: 20.h,
-            width: 390.w,
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 30.h,
+                width: 390.w,
+              ),
+              SizedBox(
+                height: 16.h,
+              ),
+              SizedBox(
+                width: 300.w,
+                child: Text(
+                  "Unlock all the power of this mobile tool and enjoy digital experience like never before!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: AppColors.textSecondary.withValues(alpha: 0.7),
+                      fontWeight: FontWeight.normal,
+                      fontSize: 14.sp),
+                ),
+              ),
+              SizedBox(
+                height: 24.h,
+              ),
+              Image.asset(
+                AppIcons.premiumOffer,
+                width: 340.w,
+                height: 164,
+                fit: BoxFit.fill,
+              ),
+              SizedBox(
+                height: 48.h,
+              ),
+              SizedBox(
+                  width: 350.w,
+                  height: 200.h,
+                  child: PremiumSubscribeList(
+                    premiumList: premium,
+                  )),
+              SizedBox(
+                height: 30.h,
+              ),
+              CustomButton(
+                  height: 50,
+                  width: 350,
+                  title: 'Start 30-day free trial',
+                  fontSize: 14,
+                  onTap: () {}),
+              SizedBox(
+                height: 30.h,
+              ),
+              SizedBox(
+                width: 350.w,
+                //height: 50.h,
+                child: Text(
+                  "By placing this order, you agree to the Terms of Service and Privacy Policy. Subscription automatically renews unless auto-renew is turned off at least 24-hours before the end of the current period.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 12.sp,
+                      fontWeight: FontWeight.w300,
+                      color: AppColors.secondary),
+                ),
+              )
+            ],
           ),
-          Text(
-            "Get Premium",
-            style: TextStyle(
-                color: AppColors.textSecondary,
-                fontWeight: FontWeight.bold,
-                fontSize: 32.sp),
-          ),
-          SizedBox(
-            height: 16.h,
-          ),
-          SizedBox(
-            width: 300.w,
-            child: Text(
-              "Unlock all the power of this mobile tool and enjoy digital experience like never before!",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  color: AppColors.textSecondary.withValues(alpha: 0.7),
-                  fontWeight: FontWeight.normal,
-                  fontSize: 14.sp),
-            ),
-          ),
-          SizedBox(
-            height: 24.h,
-          ),
-          Image.asset(
-            AppIcons.premium,
-            width: 340.w,
-            height: 164,
-            fit: BoxFit.fill,
-          ),
-          SizedBox(
-            height: 48.h,
-          ),
-          SizedBox(
-              width: 350.w,
-              height: 200.h,
-              child: PremiumSubscribeList(
-                premiumList: premium,
-              )),
-          SizedBox(
-            height: 20.h,
-          ),
-          CustomButton(
-              height: 50,
-              width: 350,
-              title: 'Start 30-day free trial',
-              fontSize: 14,
-              onTap: () {}),
-          SizedBox(
-            height: 25.h,
-          ),
-          SizedBox(
-            width: 350.w,
-            height: 50.h,
-            child: Text(
-              "By placing this order, you agree to the Terms of Service and Privacy Policy. Subscription automatically renews unless auto-renew is turned off at least 24-hours before the end of the current period.",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  fontSize: 10.sp,
-                  fontWeight: FontWeight.w300,
-                  color: AppColors.secondary),
-            ),
-          )
+          const ComingSoon(height: 844, width: 390, showAnimation: true),
         ],
       ),
     );
