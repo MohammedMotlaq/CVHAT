dd9b935296c1d5dab0077e5c967d2ae78a1a3ef7
Mohammed Abu Motlaq <85284985+MohammedMotlaq@users.noreply.github.com>
2025-03-26 20:37:28 +0200

    feat: profile model

---
+++ lib/models/profile_model.dart
@@ -0,0 +1,29 @@
+class Profile {
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String avatarURL;
+
+  Profile(
+      {required this.firstName,
+      required this.lastName,
+      required this.email,
+      required this.avatarURL});
+
+  factory Profile.fromJson(Map<String, dynamic> json) {
+    return Profile(
+        firstName: json['firstName'],
+        lastName: json['lastName'],
+        email: json['email'],
+        avatarURL: json['avatarURL']);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'firstName': firstName,
+      'lastName': lastName,
+      'email': email,
+      'avatarURL': avatarURL
+    };
+  }
+}
