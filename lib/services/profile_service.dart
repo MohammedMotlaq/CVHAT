469be20462d10b8b5d853979a02a85c8fdee771a
Mohammed Abu Motlaq <85284985+MohammedMotlaq@users.noreply.github.com>
2025-03-30 12:16:55 +0200

    add: post avatar function

--- lib/services/profile_service.dart
+++ lib/services/profile_service.dart
@@ -1,6 +1,7 @@
 import 'package:cvhat/constants/api_endpoints.dart';
 import 'package:cvhat/models/profile_model.dart';
 import 'package:dio/dio.dart';
+import 'package:file_picker/file_picker.dart';
 
 class ProfileService {
   ProfileService._();
@@ -30,6 +31,36 @@ class ProfileService {
       throw Exception("Failed to load profile");
     } catch (e) {
       throw Exception("Error fetching Profile: $e");
+    }
+  }
+
+  Future<Profile> postAvatar(String userToken, PlatformFile avatarFile) async {
+    try {
+      print("Uploading Avatar in Profile service");
+
+      MultipartFile multiPartFile = await MultipartFile.fromFile(
+        avatarFile.path!,
+        filename: avatarFile.name,
+        contentType: DioMediaType("image", "jpg"),
+      );
+      FormData data = FormData.fromMap({'avatar': multiPartFile});
+      Response response = await _dio.post(
+        ApiEndPoints.postAvatar,
+        options: Options(
+          headers: {
+            "Authorization": "Bearer $userToken",
+          },
+        ),
+        data: data,
+      );
+      print(response.toString());
+      if (response.statusCode == 200) {
+        return Profile.fromJson(response.data["data"]["profile"]);
+      } else {
+        throw Exception("Error Posting Avatar Profile!");
+      }
+    } catch (error) {
+      throw Exception(error.toString());
     }
   }
 }
