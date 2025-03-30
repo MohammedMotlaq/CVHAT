08033fe0cc0939bf226bd6f69f566b1c7ee22d13
Mohammed Abu Motlaq <85284985+MohammedMotlaq@users.noreply.github.com>
2025-03-30 19:13:28 +0200

    edit: favorite screen

--- lib/views/favorite_screen/favorite_screen.dart
+++ lib/views/favorite_screen/favorite_screen.dart
@@ -1,14 +1,22 @@
 import 'package:cvhat/app_router.dart';
 import 'package:cvhat/core/resources/app_animations.dart';
 import 'package:cvhat/core/resources/app_colors.dart';
+import 'package:cvhat/providers/reviews_provider.dart';
 import 'package:cvhat/providers/ui_provider.dart';
+import 'package:cvhat/views/home_screen/widgets/recent_reviews_list.dart';
 import 'package:cvhat/widgets/custom_app_bar.dart';
 import 'package:cvhat/widgets/empty_list_widget.dart';
+import 'package:cvhat/widgets/loader_blur_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 
 class FavoriteScreen extends StatelessWidget {
-  const FavoriteScreen({super.key});
+  FavoriteScreen({super.key}) {
+    Provider.of<ReviewsProvider>(AppRouter.navKey.currentContext!,
+            listen: false)
+        .fetchFavoriteReviews();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,13 +33,28 @@ class FavoriteScreen extends StatelessWidget {
             Provider.of<UiProvider>(context, listen: false).index = 0;
           },
         ),
-        body: const EmptyListWidget(
-          jsonFile: AppAnimations.favorite,
-          title: "No Favorites!",
-          showArrow: false,
-          width: 280,
-          height: 280,
-        ),
+        body: Consumer<ReviewsProvider>(
+            builder: (context, reviewsProvider, child) {
+          return reviewsProvider.isLoading
+              ? const Expanded(child: LoaderBlurScreen())
+              : reviewsProvider.favoriteReviews.isNotEmpty
+                  ? SizedBox(
+                      width: 390.w,
+                      height: double.infinity,
+                      child: RecentReviewsList(
+                          recentReviews: reviewsProvider.favoriteReviews,
+                          scrollable: true),
+                    )
+                  : const Expanded(
+                      child: EmptyListWidget(
+                        jsonFile: AppAnimations.favorite,
+                        title: "No Favorites!",
+                        showArrow: false,
+                        width: 280,
+                        height: 280,
+                      ),
+                    );
+        }),
       ),
     );
   }
